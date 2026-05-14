06e9def4b0a58511b787b80b9d0fb645155de990
SS25031 <ss25031@ues.edu.sv>
2026-05-13 22:32:47 -0600

    feat: agregar metodo para crear reservas

--- main.py
+++ main.py
@@ -64,8 +64,96 @@ class SistemaReservas:
 
     def hacer_reserva(self):
         #METODOD: hacer_reserva()
+        print("\n*** Usted esta creando una nueva reserva ***")
+        
+        try:
+            cantidad = int(input("Ingrese la cantidad de personas para la reserva: "))
+        except ValueError:
+            print("Error: Por favor ingrese un número válido.")
+            return # Detiene la funcion y regresa al menu
+
+        
+        mesas_adecuadas = []
+        for mesa in self.mesas:
+            if mesa.capacidad >= cantidad:
+                mesas_adecuadas.append(mesa)
+                
+        if not mesas_adecuadas: # Si la lista quedo vacia
+            print("Lo sentimos, no hay mesas con capacidad suficiente para esa cantidad de personas.")
+            return
+
+        #PEDIR Dia DE LA RESERVA
+        print("\nSeleccione el día para la reserva:")
+        print("1. Lunes | 2. Martes | 3. Miércoles | 4. Jueves | 5. Viernes | 6. Sábado | 7. Domingo")
+        dia = input("Día (1-7): ")
+        
+        if dia not in ["1", "2", "3", "4", "5", "6", "7"]:
+            print("Día no válido. Operación cancelada.")
+            return
+
+        #PEDIR TURNO Y VALIDAR CAPACIDAD MAXIMA
+        turno_valido = False
+        
+        while not turno_valido: # Repetir hasta que elija un turno con DISPO
+            print("\nSeleccione el turno:")
+            print("1. Mañana | 2. Tarde | 3. Noche | 0. Cancelar")
+            turno = input("Turno (1-3) o 0 para salir: ")
+            
+            if turno == "0":
+                print("Reserva cancelada por el usuario.")
+                return
+            elif turno not in ["1", "2", "3"]:
+                print("Turno no válido. Intente de nuevo.")
+                continue # Regresa al inicio del while
+                
+            # Contar reservas para este dia Y este turno especifico
+            reservas_en_este_turno = 0
+            for r in self.reservas:
+                if r.dia == dia and r.turno == turno:
+                    reservas_en_este_turno += 1
+            
+            # validar si el turno elegido ya alcazo las 25 reservas
+            if reservas_en_este_turno >= 25:
+                print(f"\nLo sentimos, el turno {turno} esta LLENO para este dia.")
+                print("Por favor, seleccione un turno distinto.")
+               
+            else:
+                turno_valido = True 
+
+        #BUSCAR LA MESA Y ASIGNAR
+        mesa_asignada = None 
+        
+        #Solo revisa las mesas con la capacidad correcta
+        for mesa in mesas_adecuadas:
+            mesa_ocupada = False
+            
+            # Revisar si hay choque
+            for r in self.reservas:
+                if r.dia == dia and r.turno == turno and r.mesa.numero == mesa.numero:
+                    mesa_ocupada = True
+                    break 
+            
+            if not mesa_ocupada:
+                mesa_asignada = mesa 
+                break 
+
+        #GUARDAR LA RESERVA FINAL
+        if mesa_asignada:
+            self.contador_id += 1 # Aumentar el ID único
+            # TICKET
+            nueva_reserva = Reserva(self.contador_id, dia, turno, mesa_asignada)
+            
+            # Guardar reserva
+            self.reservas.append(nueva_reserva) 
+            
+            print("\nReserva realizada con éxito!")
+            print(nueva_reserva) # Imprimir el ticket
+        else:
+            print("\nLo sentimos, no hay mesas disponibles con esa capacidad para ese día y turno.")
         print("\n*** Usted está creando una nueva reserva ***")
         pass
+
+    #--------------------------------------------------------------------------------------------------#
 
     def ver_reservas(self):
         #METODO ver_reservas()
