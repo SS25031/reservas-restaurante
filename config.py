4d5849ebca4be7f2718c4b5aaa44a4419fb32b89
SS25031 <ss25031@ues.edu.sv>
2026-06-19 00:09:22 -0600

    Agregar archivo config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+TOTAL_MESAS = 25
+reservas = []
+
+CAPACIDAD_MESAS = {
+    1: 2, 2: 2, 3: 2, 4: 2, 5: 2,           #Mesas para 2
+    6: 4, 7: 4, 8: 4, 9: 4, 10: 4,          #Mesas para 4
+    11: 6, 12: 6, 13: 6, 14: 6, 15: 6,      #Mesas para 6
+    16: 8, 17: 8, 18: 8, 19: 8, 20: 8,      #Mesas para 8
+    21: 10, 22: 10, 23: 10, 24: 10, 25: 10  #Mesas para 10
+}
